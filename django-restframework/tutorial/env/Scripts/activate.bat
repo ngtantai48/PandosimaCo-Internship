fd19683c7e55d787d11f225f8172fd171d800658
Nguyen Tan Tai <ngtantai48@gmail.com>
2023-12-05 15:57:08 +0700

    Django-rest-framework

--- django-restframework/tutorial/env/Scripts/activate.bat
+++ django-restframework/tutorial/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Data\VScode-workspace\python-projects\django_ProjectMySQL\django-restframework\tutorial\env
+set VIRTUAL_ENV=D:\Data\VScode-workspace\python-projects\django_ProjectMySQL\PandosimaCo-Internship\django-restframework\tutorial\env
 
 if not defined PROMPT set PROMPT=$P$G
 
